ce3b5041cf2dc6cbc24694cb4b1e53643f9a4120
Nadia <nadia.mohamed89@gmail.com>
2018-11-08 13:40:59 +0000

    Blog Styles Implemented

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,18 +1,20 @@
 class BlogsController < ApplicationController
-  
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs= Blog.paginate(:page => params[:page], :per_page => 5).order('created_at DESC')
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
