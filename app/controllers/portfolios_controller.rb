b58fbf190711164fc22fae756a16478177ab86fd
Nadia <nadia.mohamed89@gmail.com>
2018-11-11 14:17:36 +0000

    Portfolio Controller Updated

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -69,9 +69,8 @@ class PortfoliosController < ApplicationController
                                       :subtitle,
                                       :body,
                                       :main_image,
-                                      :thumb_image,
-                                      technologies_attributes: [:id, :name, :_destroy]
-                                     )
+                                      :thumb_image
+                                    )
   end
 
   def set_portfolio_item
